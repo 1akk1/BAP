79b028f7ac8012200c6ed1a95ac8ca49850d0ea5
Akash Raghav <94389454+1akk1@users.noreply.github.com>
2023-06-16 22:32:57 +0530

    GET and POST Axios
    Able to fetch backend data through axios and show it on UI and add delete and edit button to backend data on UI

--- BAP.js
+++ BAP.js
@@ -12,7 +12,7 @@ function addUser(e) {
     const phone = document.getElementById('phone').value;
     
     
-    // showing Newuser on website
+    // showing NewUser on website
     const newUser = document.createElement('li');
     newUser.className = 'list-group-items';
     newUser.appendChild(document.createTextNode(`${name}-${email}-${phone}`));
@@ -21,14 +21,14 @@ function addUser(e) {
     // adding delete button
     const deleteBtn = document.createElement('button');
     deleteBtn.className = 'btn btn-sm float-middle delete';
-    deleteBtn.appendChild(document.createTextNode('Delete'));
+    deleteBtn.appendChild(document.createTextNode('DeleteUser'));
     newUser.appendChild(deleteBtn);
 
 
     // adding edit button
     const editBtn = document.createElement('button');
     editBtn.className = 'btn btn-sm float-middle edit';
-    editBtn.appendChild(document.createTextNode('Edit'));
+    editBtn.appendChild(document.createTextNode('EditUser'));
     newUser.appendChild(editBtn)
 
     // add event listeners to button
@@ -46,7 +46,7 @@ function addUser(e) {
 
     // Saving data to backend
     axios
-        .post('https://crudcrud.com/api/cfff921b0d63430eb690925ded47a702/saveData',{
+        .post('https://crudcrud.com/api/6783ee1690e44a34931e4d9223861cb4/saveData',{
             name : name,
             email : email,
             phone : phone,
@@ -56,15 +56,50 @@ function addUser(e) {
 // Get request
 window.addEventListener("DOMContentLoaded", () => {
     axios
-        .get('https://crudcrud.com/api/cfff921b0d63430eb690925ded47a702/saveData')
+        .get('https://crudcrud.com/api/6783ee1690e44a34931e4d9223861cb4/saveData')
         .then(response => {
-            console.log(response)
-            for(var i=0;i<response.data.length;i++){
-                console.log(response.data[i]);
-            }
+            response.data.forEach(user => {
+                showUsersOnScreen(user);
+
+            })            
         })
         .catch(error => {
             console.error(error)
         })
 })
+// function to show backend data on UI
+function showUsersOnScreen(user){
+    const names =  user.name;
+    const emails = user.email;
+    const phones = user.phone;
 
+    const newUsers = document.createElement('li');
+    newUsers.className = 'list-group-items';
+    newUsers.appendChild(document.createTextNode(`${names}-${emails}-${phones}`));
+    users.appendChild(newUsers);
+
+    // delete button for backend data
+    const deleteBtn = document.createElement('button');
+    deleteBtn.className = 'btn btn-sm float-middle delete';
+    deleteBtn.appendChild(document.createTextNode('DeleteUser'));
+    newUsers.appendChild(deleteBtn);
+
+
+    // adding edit button for backend data
+    const editBtn = document.createElement('button');
+    editBtn.className = 'btn btn-sm float-middle edit';
+    editBtn.appendChild(document.createTextNode('EditUser'));
+    newUsers.appendChild(editBtn)
+
+    // add event listeners to button
+    deleteBtn.addEventListener('click',()=>{
+        newUsers.remove();
+    }) 
+    editBtn.addEventListener('click', () => {
+        document.getElementById('name').value = names;
+        document.getElementById('email').value = emails;
+        document.getElementById('phone').value = phones;
+        newUsers.remove();
+
+    })
+}
